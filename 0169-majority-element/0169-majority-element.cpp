6c0c330d5b6621328800dc340b3dd1bfa58cfd27
Karn <95653930+KaranDubey400@users.noreply.github.com>
2025-05-28 01:46:27 +0530

    Time: 3 ms (29.32%), Space: 28.2 MB (24.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,21 +1,8 @@
-#include <vector>
-#include <unordered_map>
-
 class Solution {
 public:
-    int majorityElement(std::vector<int>& nums) {
-      
-        unordered_map<int, int> karn; 
-        for(auto each : nums){
-            karn[each]++;
-        }
-
-        int n = nums.size();
-        for (auto const& [val, count] : karn) { 
-            if (count > n / 2) {
-                return val;
-            }
-        }
-        return -1; // Should not reach here if a majority element always exists as per problem constraints
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin() , nums.end()); 
+     int n = nums.size();
+        return nums[n/2]; 
     }
 };
