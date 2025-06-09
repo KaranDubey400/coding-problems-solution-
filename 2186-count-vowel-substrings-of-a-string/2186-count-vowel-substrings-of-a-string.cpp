f27bb88454dde93bf2e74746b0bdd66900d1fbab
Karn <95653930+KaranDubey400@users.noreply.github.com>
2025-06-09 23:20:07 +0530

    Time: 6 ms (40.76%), Space: 10 MB (28.67%) - LeetHub

---
+++ 2186-count-vowel-substrings-of-a-string/2186-count-vowel-substrings-of-a-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+    
+    int countVowelSubstrings(string word) {
+        int n = word.length();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!isVowel(word[i])) continue;
+
+            unordered_set<char> vowels;
+            for (int j = i; j < n && isVowel(word[j]); j++) {
+                vowels.insert(word[j]);
+                if (vowels.size() == 5) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
