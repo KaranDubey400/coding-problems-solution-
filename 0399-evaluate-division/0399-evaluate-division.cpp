03e8adbb5b20aa5807464691297cad9c5b851688
Karn <95653930+KaranDubey400@users.noreply.github.com>
2025-07-07 02:21:40 +0530

    Time: 0 ms (100%), Space: 12 MB (56.2%) - LeetHub

---
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    unordered_map<string, vector<pair<string, double>>> graph;
+
+    double dfs(string src, string dst, unordered_set<string>& visited) {
+        if (graph.find(src) == graph.end() || graph.find(dst) == graph.end()) return -1.0;
+        if (src == dst) return 1.0;
+        visited.insert(src);
+
+        for (auto& neighbor : graph[src]) {
+            string nextNode = neighbor.first;
+            double weight = neighbor.second;
+            if (!visited.count(nextNode)) {
+                double res = dfs(nextNode, dst, visited);
+                if (res != -1.0) return res * weight;
+            }
+        }
+        return -1.0;
+    }
+
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        // Step 1: Build the graph
+        for (int i = 0; i < equations.size(); ++i) {
+            string a = equations[i][0];
+            string b = equations[i][1];
+            double val = values[i];
+
+            graph[a].push_back({b, val});
+            graph[b].push_back({a, 1.0 / val});
+        }
+
+        // Step 2: Answer queries using DFS
+        vector<double> result;
+        for (auto& query : queries) {
+            unordered_set<string> visited;
+            double res = dfs(query[0], query[1], visited);
+            result.push_back(res);
+        }
+        return result;
+    }
+};
