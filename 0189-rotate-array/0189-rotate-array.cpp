116d79d7c1fc26910a8f123c0462c64135ce1677
Karn <95653930+KaranDubey400@users.noreply.github.com>
2025-08-03 23:23:53 +0530

    Time: 0 ms (100%), Space: 29.4 MB (80.83%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,19 +1,14 @@
-#include <vector>
-#include <algorithm> // For reverse function
-
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
+    void rotate(std::vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n; // Handle cases where k > n
+      
+        k = k % n;
         
-        // Approach 1: Using extra space (corrected version of your approach)
-        vector<int> temp(n);
-        
-        for (int i = 0; i < n; i++) {
-            temp[(i + k) % n] = nums[i];
-        }
-        
-        nums = temp;
+        reverse(nums.begin(), nums.end());
+         
+        reverse(nums.begin(), nums.begin() + k);
+         
+        reverse(nums.begin() + k, nums.end());
     }
 };
