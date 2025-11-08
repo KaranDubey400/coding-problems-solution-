56d3784943f9600cf3debf23fcd323ad7da74679
Karn <95653930+KaranDubey400@users.noreply.github.com>
2025-11-08 20:56:30 +0530

    Time: 0 ms (100%), Space: 20.6 MB (83.06%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xo = 0; 
-        for(int i =0 ; i < nums.size(); i++){
-              xo ^= nums[i]; 
+        int number = 0 ; 
+
+        for(auto each : nums){
+            number = number^each; 
         }
 
-        return xo; 
+        return number ; 
     }
 };
