0b49d840f3a82c177411a0d05a2ceda341f1381a
Karn <95653930+KaranDubey400@users.noreply.github.com>
2025-11-10 14:00:06 +0530

    Time: 0 ms (100%), Space: 28.3 MB (7.95%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin() , nums.end()); 
-     int n = nums.size();
-        return nums[n/2]; 
+        unordered_map<int,int> karn; 
+  int n = nums.size(); 
+
+        for(int i=0; i < n ; i++){
+             karn[nums[i]]++; 
+        }
+
+        int p = n/2; 
+
+       for(auto each: karn){
+        if(each.second > p ){
+            return each.first;
+        }
+       }
+     return 0 ; 
     }
 };
