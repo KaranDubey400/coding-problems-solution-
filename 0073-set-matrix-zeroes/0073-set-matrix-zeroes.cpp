b0470a8008644deb7afddaffb7a8b58796a93824
Karn <95653930+KaranDubey400@users.noreply.github.com>
2025-11-19 01:25:28 +0530

    Time: 0 ms (100%), Space: 18.6 MB (51.29%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int row = matrix.size();
-        int col = matrix[0].size();
-        
-        vector<bool> rowZero(row, false);
-        vector<bool> colZero(col, false);
-        
-        // First pass: mark which rows and columns need to be zeroed
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+      
+        vector<bool> row(m, false);
+        vector<bool> col(n, false);
+       
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
                 if(matrix[i][j] == 0) {
-                    rowZero[i] = true;
-                    colZero[j] = true;
+                    row[i] = true;
+                    col[j] = true;
                 }
             }
         }
         
-        // Second pass: set zeros based on markers
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if(rowZero[i] || colZero[j]) {
+      
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(row[i] || col[j]) {
                     matrix[i][j] = 0;
                 }
             }
