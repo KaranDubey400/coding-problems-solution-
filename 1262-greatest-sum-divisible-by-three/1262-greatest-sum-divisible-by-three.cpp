5421136df35a3361ca8933e62dd7b1ba4401a49a
Karn <95653930+KaranDubey400@users.noreply.github.com>
2025-11-23 21:38:58 +0530

    Time: 38 ms (41.49%), Space: 37.1 MB (35.93%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,41 +1,17 @@
 class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
-        int sum = 0;
-     
-        vector<int> rem1, rem2;
+        vector<int> dp = {0, INT_MIN, INT_MIN};
         
         for (int num : nums) {
-            sum += num;
-            if (num % 3 == 1) rem1.push_back(num);
-            if (num % 3 == 2) rem2.push_back(num);
+            vector<int> temp = dp;
+            for (int i = 0; i < 3; i++) {
+                if (temp[i] != INT_MIN) {
+                    dp[(i + num) % 3] = max(dp[(i + num) % 3], temp[i] + num);
+                }
+            }
         }
         
-      
-        sort(rem1.begin(), rem1.end());
-        sort(rem2.begin(), rem2.end());
-        
-        if (sum % 3 == 0) return sum;
-        
-        int result = 0;
-        
-        if (sum % 3 == 1) {
-           
-            if (!rem1.empty()) 
-                result = max(result, sum - rem1[0]);
-          
-            if (rem2.size() >= 2) 
-                result = max(result, sum - rem2[0] - rem2[1]);
-        } 
-        else if (sum % 3 == 2) {
-           
-            if (!rem2.empty()) 
-                result = max(result, sum - rem2[0]);
-          
-            if (rem1.size() >= 2) 
-                result = max(result, sum - rem1[0] - rem1[1]);
-        }
-        
-        return result;
+        return dp[0];
     }
 };
