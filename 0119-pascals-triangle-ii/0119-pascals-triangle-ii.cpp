f9704a9a0ed7bd05198806a478c1d61cb4bb6c3b
Karn <95653930+KaranDubey400@users.noreply.github.com>
2025-11-27 15:58:19 +0530

    Time: 0 ms (100%), Space: 9 MB (32.18%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -6,11 +6,10 @@ public:
         long long val = 1;
         row.push_back(val);
         
-      
-        for (int k = 1; k <= N; k++) {
-            val = val * (N - k + 1) / k;
-            row.push_back(val);
-        }
+      for(int i = 1; i < N+1; i++){
+        val = val * (N - i +1)  / i; 
+        row.push_back(val);
+      }
         
         return row;
     }
